70524435bcb3e076ac9298ef18421f20586d0170
navid <reza.navid@hotmail.ca>
2021-03-13 21:50:13 -0500

    Completed heuristic function 1

--- main.py
+++ main.py
@@ -356,20 +356,190 @@ class IterativeDeepening:
                templist.extend([left,down,right,up])
         
         return templist
+
+class Aster:
+
+    def __init__(self, start, goal):
+        self.start = start
+        self.depth = 0
+        self.goal = goal
+        self.path = []
+        self.order = []
+        self.closed = []
+        self.opened = []
+        self.numericalDict = dict()
+        count = 1
+        rowc = 0
+        for row in goal:
+            colc = 0
+            for placement in row:
+                self.order.append(placement)
+                self.numericalDict[count] = [rowc,colc]
+                colc+=1
+                count+=1
+            rowc+=1
+
+    def swap(self,orgstate, newstate,start):
+        val1 = start[orgstate[0]][orgstate[1]]
+        val2 = start[newstate[0]][newstate[1]]
+        start[orgstate[0]][orgstate[1]] = val2
+        start[newstate[0]][newstate[1]] = val1
+
+    def manhattan(self,curpos,goalpos):
+        distance = abs(curpos[0] - goalpos[0]) + abs(curpos[1] - goalpos[1])
+        return distance
+
+    def heuristic1(self,currentNum,currentState,start,originalstate,isFound,mandist):
+        newmandist = self.manhattan(currentState,self.numericalDict[currentNum])
+        if mandist >= newmandist:
+            if len(self.closed) == 0:
+                currentState = self.opened.pop(0) # the target state that you wish to reach
+                self.path.append(currentState)
+                self.closed = originalstate
+                self.closed.append(currentState)
+                movelist = self.categorize(currentState[0],currentState[1])
+                if self.numericalDict[currentNum] == currentState:
+                    isFound[0] = True
+            else:
+                movelist = self.categorize(currentState[0],currentState[1])
+        
+            while len(movelist) != 0:
+                forcount = 0
+                for move in movelist:
+                    if not isFound[0]:
+                        newstart = copy.deepcopy(start)
+                        if move in self.closed:
+                            forcount+=1
+                        else:
+                            self.swap(currentState,move,newstart)
+                            self.path.append(move)
+
+                            if self.numericalDict[currentNum] == move:
+                                isFound[0] = True
+                                self.start = newstart
+                                break
+                            else:
+                                while forcount > 0:
+                                    movelist.pop(0)
+                                    forcount-=1
+                                closed_node = movelist.pop(0)
+                                if closed_node not in self.closed:
+                                    self.closed.append(closed_node)
+                                self.heuristic1(currentNum,closed_node,newstart,originalstate,isFound,newmandist)
+                                if not isFound[0]:
+                                    self.path.append(currentState)
+                        if forcount == len(movelist):
+                            movelist = list()
+                    else:
+                        movelist = list()     
+        else:
+            if currentState not in self.closed:
+                self.closed.append(currentState)
+
+
+
+    def phoenix(self): # heuristic 1 : the manhattan distance
+        permlist = list()
+        counting = 1
+        starttime = time.time()
+        print("starting h1...")
+        while len(self.order)> 0:
+
+            target = self.order.pop(0)
+
+            rowind = 0
+            colind = 0
+
+            found =False
+            for row in self.start:
+                for loc in row:
+                    if loc == target:
+                        rowind = self.start.index(row)
+                        colind = row.index(loc)
+                        found = True
+                        break
+                if found:
+                    break
+            found = [False]
+            self.opened.append([rowind,colind])
+            currDist = self.manhattan([rowind,colind],self.numericalDict[counting])
+            self.heuristic1(target,[rowind,colind],self.start,permlist,found,currDist)
+            self.opened = list()
+            self.closed = list()
+            self.path = list()
+            permlist = list()
+            counting+=1
+            for i in range(1,counting):
+                permlist.append(self.numericalDict[i])
+        endtime = time.time()
+        print("ended with time : " + str((endtime -starttime)))
+
+
+    def categorize(self,rowind,colind):
+        templist = list()
+        if rowind == 0:
+
+            if colind == 0:
+                down = [rowind+1,colind]
+                right = [rowind,colind+1]
+                templist.extend([down,right])
+            elif colind == len(start)-1:
+                left = [rowind,colind-1]
+                down = [rowind+1,colind]
+                templist.extend([left,down])
+            else:
+                left = [rowind,colind-1]
+                down = [rowind+1,colind]
+                right = [rowind,colind+1]
+                templist.extend([left,down,right])
+
+        elif rowind == len(start)-1:
+
+            if colind == 0:
+                right = [rowind,colind+1]
+                up = [rowind-1,colind]
+                templist.extend([right,up])
+            elif colind == len(start)-1:
+                left = [rowind,colind-1]
+                up = [rowind-1,colind]
+                templist.extend([left,up])
+            else:
+                left = [rowind,colind-1]
+                right = [rowind,colind+1]
+                up = [rowind-1,colind]
+                templist.extend([left,right,up])
+
+        else:
+            
+            if colind == 0:
+               down = [rowind+1,colind]
+               right = [rowind,colind+1]
+               up = [rowind-1,colind]
+               templist.extend([down,right,up])
+            elif colind == len(start)-1:
+               left = [rowind,colind-1] 
+               down = [rowind+1,colind]
+               up = [rowind-1,colind]
+               templist.extend([left,down,up])
+            else:
+               left = [rowind,colind-1] 
+               down = [rowind+1,colind]
+               right = [rowind,colind+1]
+               up = [rowind-1,colind]
+               templist.extend([left,down,right,up])
+        
+        return templist
+
             
 
     
 
 
-# def RecurseTest(integer):
-#     if integer > 1:
-#         RecurseTest(integer-1)
-#     print(integer)
 
 n = int(input("Enter the n value : "))
 file = open('random.txt', 'r')
 Lines = file.readlines()
-goals = list()
+starts = list()
 
 for line in Lines:
     line.strip()
@@ -384,18 +554,21 @@ for line in Lines:
             newgoal.append(subgoal)
             count = 1
             subgoal = list()
-    goals.append(newgoal)
+    starts.append(newgoal)
 
 count = 0
-for goal in goals:
+for start in starts:
     print("iteration " + str(count))
     idf = IterativeDeepening(start,goal)
     idf.iterate()
 
     dfs = DFS(start,goal)
     dfs.iterate()
-    print("\n")
     count+=1
+
+    aster  = Aster(start,goal)
+    aster.phoenix()
+    print("\n")
 
 #RecurseTest(integer=10)
 
